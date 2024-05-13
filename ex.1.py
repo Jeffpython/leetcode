889b2efe13660cc630d5fe7eb9885eadd9aeed9a
Jeff <jeffwingchun@mail.ru>
2024-05-13 21:22:00 +0300

    add ex.1

---
+++ ex.1.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/explore/learn/card/fun-with-arrays/521/introduction/3238/
+# Max Consecutive Ones
+# Given a binary array nums, return the maximum number of consecutive 1's in the array.
+
+
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        max_consecutive_ones = 0
+        current_consecutive_ones = 0
+
+        for i in range(len(nums)):
+            if nums[i] == 1:
+                if current_consecutive_ones == 0:
+                    current_consecutive_ones = 1
+                elif i > 0 and nums[i - 1] == 1:
+                    current_consecutive_ones += 1
+
+                if current_consecutive_ones > max_consecutive_ones:
+                    max_consecutive_ones = current_consecutive_ones
+            else:
+                current_consecutive_ones = 0
+
+        return max_consecutive_ones
