8c2e6286b5d7357af0df92809d3a78a4e7db8c0d
Jeff <jeffwingchun@mail.ru>
2024-07-21 17:52:29 +0300

    completion 26.py

--- 26.py
+++ 26.py
@@ -14,11 +14,18 @@
 # respectively.
 # It does not matter what you leave beyond the returned k (hence they are underscores).
 
-# class Solution:
-#     def removeDuplicates(self, nums: List[int]) -> int:
 
-nums = [1,1,2]
-result = []
+class Solution:
+    def removeDuplicates(self, nums: list[int]) -> int:
 
-for i in range(len(nums)):
-    pass
+        if len(nums) == 0:
+            return 0
+
+        j = 1
+
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i-1]:
+                nums[j] = nums[i]
+                j += 1
+
+        return j
